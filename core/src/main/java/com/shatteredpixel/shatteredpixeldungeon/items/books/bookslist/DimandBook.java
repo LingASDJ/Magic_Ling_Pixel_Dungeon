82070b7bfe0f6ebd486a59e1de1752ceaa8f061e
LingASDJ <2735951230@qq.com>
2023-06-08 13:13:28 +0800

    Update V0.6.2.0-Beta2.0-Patch1

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/books/bookslist/DimandBook.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/books/bookslist/DimandBook.java
@@ -18,7 +18,7 @@ public class DimandBook extends Books {
     @Override
     public void execute(final Hero hero, String action) {
         super.execute(hero, action);
-        if (action.equals( Read ) && quantity>1) {
+        if (action.equals( Read )) {
             Sample.INSTANCE.play( Assets.Sounds.READ );
             detach( hero.belongings.backpack );
             GLog.n("你已接受来自未知敌人的挑战……，祝你好运！");
