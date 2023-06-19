2c4f570e36d7229492b9d61cb3232820836333c3
LingASDJ <2735951230@qq.com>
2023-06-19 22:48:31 +0800

    Clear

--- services/analytics/com/shatteredpixel/shatteredpixeldungeon/services/analytics/AnalyticsBelongingsData.java
+++
@@ -1,8 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.services.analytics;
-
-import java.util.LinkedHashMap;
-
-public class AnalyticsBelongingsData {
-    public boolean cheater = false;
-    public LinkedHashMap<String, Integer> items;
-}
