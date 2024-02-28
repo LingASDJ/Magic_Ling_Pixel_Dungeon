c6343de205967bdb6f3dab131fde292416893ac5
zxcPandora <1158500986@qq.com>
2024-02-28 11:52:08 +0800

    数字输入框完善

--- SPD-classes/src/main/java/com/watabou/noosa/TextInput.java
+++ SPD-classes/src/main/java/com/watabou/noosa/TextInput.java
@@ -33,6 +33,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.watabou.glscripts.Script;
 import com.watabou.glwrap.Blending;
@@ -228,5 +229,9 @@ public class TextInput extends Component {
 
 	public void setCursorPosition (int cursorPosition){
 		textField.setCursorPosition(cursorPosition);
+	}
+
+	public void setTextFieldListener(@Null TextField.TextFieldListener listener){
+		textField.setTextFieldListener(listener);
 	}
 }
