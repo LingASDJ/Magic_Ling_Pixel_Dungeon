a364643273a8ed9ddaf448276128b9d42b3f7b10
JDSA Ling <70191651+LingASDJ@users.noreply.github.com>
2025-07-22 18:44:07 +0800

    修复圣水瓶对人形态的吸血鬼使用后导致游戏进入循环

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/hollow/Vampire.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/hollow/Vampire.java
@@ -4,6 +4,7 @@ import static com.shatteredpixel.shatteredpixeldungeon.Dungeon.hero;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Bleeding;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ElementalBuff.BaseBuff.ScaryBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Poison;
@@ -55,7 +56,8 @@ public class Vampire extends Mob {
 
         if(holy){
             godDied();
-            die(true);
+            HP = 1;
+            Buff.affect(this, Bleeding.class).set(2f);
         }
 
         return super.act();
