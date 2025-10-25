b538274c5cebf1f19dfea2fa441555b809f361e2
JDSA Ling <CutelinXXX@126.com>
2025-10-25 16:14:01 +0800

    Delete Html Support So It Brach WebTest

--- html/buildTestWeb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# ./gradlew clean html:build --refresh-dependencies --no-daemon
-
-../gradlew :html:build
-wait
-if [[ -d ../release/webapp/ ]]; then
-  python3 -m http.server 8000 -d ../release/webapp/
-fi
