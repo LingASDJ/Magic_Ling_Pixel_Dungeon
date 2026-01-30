26169a57b68c6d58afc272b33f8f786ac03a95e0
JDSA Ling <CutelinXXX@126.com>
2026-01-30 15:13:12 +0800

    修复br部分楼层判定不是坚实地面导致的异常

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/traps/PitfallTrap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/traps/PitfallTrap.java
@@ -50,7 +50,7 @@ public class PitfallTrap extends Trap {
 	@Override
 	public void activate() {
 		
-		if( Dungeon.bossLevel() || Dungeon.depth > 25 || Dungeon.branch != 0){
+		if( Dungeon.RushBossLevel()|| Dungeon.bossLevel() || Dungeon.depth > 25 || Dungeon.branch != 0){
 			GLog.w(Messages.get(this, "no_pit"));
 			return;
 		}
