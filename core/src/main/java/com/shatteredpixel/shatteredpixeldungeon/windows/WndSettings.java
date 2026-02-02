71e4c0c426a3a66a3f2b76092db534aa923c432d
JDSA Ling <CutelinXXX@126.com>
2026-02-02 21:33:20 +0800

    Update WndSettings.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndSettings.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndSettings.java
@@ -1364,12 +1364,13 @@ public class WndSettings extends WndTabbed {
 
 	private static class LangsTab extends Component{
 
-		final static int COLS_P = 1;
-		final static int COLS_L = 2;
+		final static int COLS_P = 3;
+		final static int COLS_L = 6;
+
+		final static int BTN_HEIGHT = 11;
 
 		RenderedTextBlock title;
 		ColorBlock sep1;
-		RenderedTextBlock txtLangName;
 		RenderedTextBlock txtLangInfo;
 		ColorBlock sep2;
 		RedButton[] lanBtns;
@@ -1395,17 +1396,14 @@ public class WndSettings extends WndTabbed {
 
 			final Languages currLang = Messages.lang();
 
-			txtLangName = PixelScene.renderTextBlock( Messages.titleCase(currLang.nativeName()) , 9 );
-			if (currLang.status() == Languages.Status.REVIEWED) txtLangName.hardlight(TITLE_COLOR);
-			else if (currLang.status() == Languages.Status.UNREVIEWED) txtLangName.hardlight(CharSprite.WARNING);
-			else if (currLang.status() == Languages.Status.INCOMPLETE) txtLangName.hardlight(CharSprite.NEGATIVE);
-			add(txtLangName);
-
 			txtLangInfo = PixelScene.renderTextBlock(6);
-			if (currLang == Languages.CHINESE) txtLangInfo.text(Messages.get(WndSettings.class,"origin_language"));
-			else if (currLang.status() == Languages.Status.REVIEWED) txtLangInfo.text(Messages.get(this, "completed"));
-			else if (currLang.status() == Languages.Status.UNREVIEWED) txtLangInfo.text(Messages.get(this, "unreviewed"));
-			else if (currLang.status() == Languages.Status.INCOMPLETE) txtLangInfo.text(Messages.get(this, "unfinished"));
+			String info = "_" + Messages.titleCase(currLang.nativeName()) + "_ - ";
+			if (currLang == Languages.ENGLISH) info += "This is the source language, written by the developer.";
+			else if (currLang.status() == Languages.Status.REVIEWED) info += Messages.get(this, "completed");
+			else if (currLang.status() == Languages.Status.UNREVIEWED) info += Messages.get(this, "unreviewed");
+			else if (currLang.status() == Languages.Status.INCOMPLETE) info += Messages.get(this, "unfinished");
+			txtLangInfo.text(info);
+
 			if (currLang.status() == Languages.Status.UNREVIEWED) txtLangInfo.setHightlighting(true, CharSprite.WARNING);
 			else if (currLang.status() == Languages.Status.INCOMPLETE) txtLangInfo.setHightlighting(true, CharSprite.NEGATIVE);
 			add(txtLangInfo);
@@ -1416,7 +1414,7 @@ public class WndSettings extends WndTabbed {
 			lanBtns = new RedButton[langs.size()];
 			for (int i = 0; i < langs.size(); i++){
 				final int langIndex = i;
-				RedButton btn = new RedButton(Messages.titleCase(langs.get(i).nativeName()), 8){
+				RedButton btn = new RedButton(Messages.titleCase(langs.get(i).nativeName()), 6){
 					@Override
 					protected void onClick() {
 						super.onClick();
@@ -1454,7 +1452,7 @@ public class WndSettings extends WndTabbed {
 			sep3 = new ColorBlock(1, 1, 0xFF000000);
 			add(sep3);
 
-			txtTranifex = PixelScene.renderTextBlock(6);
+			txtTranifex = PixelScene.renderTextBlock(5);
 			txtTranifex.text(Messages.get(this, "transifex"));
 			add(txtTranifex);
 
@@ -1464,64 +1462,73 @@ public class WndSettings extends WndTabbed {
 					@Override
 					protected void onClick() {
 						super.onClick();
-						String creds = "";
-						String creds2 = "";
 						String[] reviewers = currLang.reviewers();
 						String[] translators = currLang.translators();
 
-						ArrayList<String> total = new ArrayList<>();
-						total.addAll(Arrays.asList(reviewers));
-						total.addAll(Arrays.asList(reviewers));
-						total.addAll(Arrays.asList(translators));
-						int translatorIdx = reviewers.length;
+						int totalCredits = 2*reviewers.length + translators.length;
+						int totalTokens = 2*totalCredits; //for spaces
 
-						//we have 2 columns in wide mode
-						boolean wide = (2 * reviewers.length + translators.length) > (PixelScene.landscape() ? 15 : 30);
+						//additional space for titles, and newline chars
+						if (reviewers.length > 0) totalTokens+=6;
+						totalTokens +=4;
 
-						int i;
-						if (reviewers.length > 0) {
-							creds += Messages.titleCase(Messages.get(LangsTab.this, "reviewers"));
-							creds2 += "";
-							boolean col2 = false;
-							for (i = 0; i < total.size(); i++) {
-								if (i == translatorIdx){
-									creds += "\n\n" + Messages.titleCase(Messages.get(LangsTab.this, "translators"));
-									creds2 += "\n\n";
-									if (col2) creds2 += "\n";
-									col2 = false;
-								}
-								if (wide && col2) {
-									creds2 += "\n-" + total.get(i);
-								} else {
-									creds += "\n-" + total.get(i);
-								}
-								col2 = !col2 && wide;
+						String[] entries = new String[totalTokens];
+						int index = 0;
+						if (reviewers.length > 0){
+							entries[0] = "_";
+							entries[1] = Messages.titleCase(Messages.get(LangsTab.this, "reviewers"));
+							entries[2] = "_";
+							entries[3] = "\n";
+							index = 4;
+							for (int i = 0; i < reviewers.length; i++){
+								entries[index] = reviewers[i];
+								if (i < reviewers.length-1) entries[index] += ", ";
+								entries[index+1] = " ";
+								index += 2;
 							}
+							entries[index] = "\n";
+							entries[index+1] = "\n";
+							index += 2;
 						}
 
-						Window credits = new Window(0, 0, 0, Chrome.get(Chrome.Type.TOAST));
+						entries[index] = "_";
+						entries[index+1] = Messages.titleCase(Messages.get(LangsTab.this, "translators"));
+						entries[index+2] = "_";
+						entries[index+3] = "\n";
+						index += 4;
 
-						int w = wide ? 125 : 60;
+						//reviewers are also shown as translators
+						for (int i = 0; i < reviewers.length; i++){
+							entries[index] = reviewers[i];
+							if (i < reviewers.length-1 || translators.length > 0) entries[index] += ", ";
+							entries[index+1] = " ";
+							index += 2;
+						}
 
-						RenderedTextBlock title = PixelScene.renderTextBlock(6);
+						for (int i = 0; i < translators.length; i++){
+							entries[index] = translators[i];
+							if (i < translators.length-1) entries[index] += ", ";
+							entries[index+1] = " ";
+							index += 2;
+						}
+
+						Window credits = new Window(0, 0, Chrome.get(Chrome.Type.TOAST));
+
+						int w = PixelScene.landscape() ? 120 : 80;
+						if (totalCredits >= 25) w *= 1.5f;
+
+						RenderedTextBlock title = PixelScene.renderTextBlock(9);
 						title.text(Messages.titleCase(Messages.get(LangsTab.this, "credits")), w);
-						title.hardlight(SHPX_COLOR);
+						title.hardlight(TITLE_COLOR);
 						title.setPos((w - title.width()) / 2, 0);
 						credits.add(title);
 
-						RenderedTextBlock text = PixelScene.renderTextBlock(5);
-						text.setHightlighting(false);
-						text.text(creds, 65);
-						text.setPos(0, title.bottom() + 2);
-						credits.add(text);
+						RenderedTextBlock text = PixelScene.renderTextBlock(7);
+						text.maxWidth(w);
+						text.tokens(entries);
 
-						if (wide) {
-							RenderedTextBlock rightColumn = PixelScene.renderTextBlock(5);
-							rightColumn.setHightlighting(false);
-							rightColumn.text(creds2, 65);
-							rightColumn.setPos(65, title.bottom() + 6);
-							credits.add(rightColumn);
-						}
+						text.setPos(0, title.bottom() + 4);
+						credits.add(text);
 
 						credits.resize(w, (int) text.bottom() + 2);
 						ShatteredPixelDungeon.scene().addToFront(credits);
@@ -1536,15 +1543,12 @@ public class WndSettings extends WndTabbed {
 		protected void layout() {
 			title.setPos((width - title.width())/2, y + GAP);
 			sep1.size(width, 1);
-			sep1.y = title.bottom() + 2*GAP;
+			sep1.y = title.bottom() + 3*GAP;
 
-			txtLangName.setPos( (width - txtLangName.width())/2f, sep1.y + 1 + GAP );
-			PixelScene.align(txtLangName);
-
-			txtLangInfo.setPos(0, txtLangName.bottom() + 2*GAP);
+			txtLangInfo.setPos(0, sep1.y + 1 + GAP);
 			txtLangInfo.maxWidth((int)width);
 
-			y = txtLangInfo.bottom() + GAP;
+			y = txtLangInfo.bottom() + 2*GAP;
 			int x = 0;
 
 			sep2.size(width, 1);
