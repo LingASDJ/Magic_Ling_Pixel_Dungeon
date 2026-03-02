c494b4718f70e7a32bc335a12f68634f9848ec8f
JDSA Ling <CutelinXXX@126.com>
2026-03-02 18:54:00 +0800

    新1阶混沌藏品：梦之种

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/props/DreamSeed.java
@@ -0,0 +1,11 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.props;
+
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class DreamSeed extends Prop {
+    {
+        rareness = 0;
+        kind = 2;
+        image = ItemSpriteSheet.DREAM_SEED;
+    }
+}
