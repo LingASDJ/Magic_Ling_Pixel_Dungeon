b4803841002681d9bfa209db098f5473f902bbaf
JDSA Ling <CutelinXXX@126.com>
2026-03-20 17:28:46 +0800

    v0.9.4：优化异果层判定，未锁定情况下可以召唤盟友

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/nosync/ForestHardBossLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/nosync/ForestHardBossLevel.java
@@ -167,9 +167,11 @@ public class ForestHardBossLevel extends Level {
                 mobs[0].HP = 60;
             }
         } else {
-            for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
-                if (mob.alignment == Char.Alignment.ALLY) {
-                    mob.die(null);
+            if(locked){
+                for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
+                    if (mob.alignment == Char.Alignment.ALLY) {
+                        mob.die(null);
+                    }
                 }
             }
         }
