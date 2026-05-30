4241bec482c95d129548a22777e388e9b29ba5b8
JDSA Ling <CutelinXXX@126.com>
2026-05-30 20:51:24 +0800

    v0.9.4.6：肃杀+炸弹匕首武技制作

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/BoomSword.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/BoomSword.java
@@ -4,9 +4,13 @@ import static com.shatteredpixel.shatteredpixeldungeon.Dungeon.hero;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ArcaneBomb;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Bomb;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Firebomb;
@@ -19,12 +23,15 @@ import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ShrapnelBomb;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.CellSelector;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.MissileSprite;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.BArray;
 import com.watabou.utils.Callback;
+import com.watabou.utils.PathFinder;
 import com.watabou.utils.Random;
 
 import java.util.ArrayList;
@@ -230,4 +237,69 @@ public class BoomSword extends MeleeWeapon {
             nextBomb = generateBomb();
         }
     }
+
+
+    @Override
+    public String targetingPrompt() {
+        return Messages.get(this, "prompt");
+    }
+
+    public boolean useTargeting(){
+        return false;
+    }
+
+    @Override
+    protected void duelistAbility(Hero hero, Integer target) {
+        sneakAbility(hero, target, 5, 2+buffedLvl(), this);
+    }
+
+    @Override
+    public String abilityInfo() {
+        if (levelKnown){
+            return Messages.get(this, "ability_desc", 2+buffedLvl());
+        } else {
+            return Messages.get(this, "typical_ability_desc", 2);
+        }
+    }
+
+    @Override
+    public String upgradeAbilityStat(int level) {
+        return Integer.toString(2+level);
+    }
+
+    public static void sneakAbility(Hero hero, Integer target, int maxDist, int invisTurns, MeleeWeapon wep){
+        if (target == null) {
+            return;
+        }
+
+        PathFinder.buildDistanceMap(Dungeon.hero.pos, BArray.or(Dungeon.level.passable, Dungeon.level.avoid, null), maxDist);
+        if (PathFinder.distance[target] == Integer.MAX_VALUE || !Dungeon.level.heroFOV[target] || hero.rooted) {
+            GLog.w(Messages.get(wep, "ability_target_range"));
+            if (Dungeon.hero.rooted) PixelScene.shake( 1, 1f );
+            return;
+        }
+
+        if (Actor.findChar(target) != null) {
+            GLog.w(Messages.get(wep, "ability_occupied"));
+            return;
+        }
+
+        wep.beforeAbilityUsed(hero, null);
+        Buff.affect(hero, Invisibility.class, invisTurns-1); //1 fewer turns as ability is instant
+
+        Dungeon.hero.sprite.turnTo( Dungeon.hero.pos, target);
+        Dungeon.hero.pos = target;
+        Dungeon.level.occupyCell(Dungeon.hero);
+        Dungeon.observe();
+        GameScene.updateFog();
+        Dungeon.hero.checkVisibleMobs();
+
+        Dungeon.hero.sprite.place( Dungeon.hero.pos );
+        CellEmitter.get( Dungeon.hero.pos ).burst( Speck.factory( Speck.WOOL ), 6 );
+        Sample.INSTANCE.play( Assets.Sounds.PUFF );
+
+        hero.next();
+        wep.afterAbilityUsed(hero);
+    }
+
 }
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/legend/SaiPlus.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/legend/SaiPlus.java
@@ -2,15 +2,26 @@ package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.legend;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Badges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.enchantments.Grim;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.BArray;
+import com.watabou.utils.PathFinder;
 import com.watabou.utils.Random;
 
 public class SaiPlus extends MeleeWeapon {
@@ -112,6 +123,69 @@ public class SaiPlus extends MeleeWeapon {
             return (int) ((175 + tier*25) * 0.9f);
         }
         return 175 + tier*25;
+    }
+
+    @Override
+    public String targetingPrompt() {
+        return Messages.get(this, "prompt");
+    }
+
+    public boolean useTargeting(){
+        return false;
+    }
+
+    @Override
+    protected void duelistAbility(Hero hero, Integer target) {
+        sneakAbility(hero, target, 5, 2+buffedLvl(), this);
+    }
+
+    @Override
+    public String abilityInfo() {
+        if (levelKnown){
+            return Messages.get(this, "ability_desc", 2+buffedLvl());
+        } else {
+            return Messages.get(this, "typical_ability_desc", 2);
+        }
+    }
+
+    @Override
+    public String upgradeAbilityStat(int level) {
+        return Integer.toString(2+level);
+    }
+
+    public static void sneakAbility(Hero hero, Integer target, int maxDist, int invisTurns, MeleeWeapon wep){
+        if (target == null) {
+            return;
+        }
+
+        PathFinder.buildDistanceMap(Dungeon.hero.pos, BArray.or(Dungeon.level.passable, Dungeon.level.avoid, null), maxDist);
+        if (PathFinder.distance[target] == Integer.MAX_VALUE || !Dungeon.level.heroFOV[target] || hero.rooted) {
+            GLog.w(Messages.get(wep, "ability_target_range"));
+            if (Dungeon.hero.rooted) PixelScene.shake( 1, 1f );
+            return;
+        }
+
+        if (Actor.findChar(target) != null) {
+            GLog.w(Messages.get(wep, "ability_occupied"));
+            return;
+        }
+
+        wep.beforeAbilityUsed(hero, null);
+        Buff.affect(hero, Invisibility.class, invisTurns-1); //1 fewer turns as ability is instant
+
+        Dungeon.hero.sprite.turnTo( Dungeon.hero.pos, target);
+        Dungeon.hero.pos = target;
+        Dungeon.level.occupyCell(Dungeon.hero);
+        Dungeon.observe();
+        GameScene.updateFog();
+        Dungeon.hero.checkVisibleMobs();
+
+        Dungeon.hero.sprite.place( Dungeon.hero.pos );
+        CellEmitter.get( Dungeon.hero.pos ).burst( Speck.factory( Speck.WOOL ), 6 );
+        Sample.INSTANCE.play( Assets.Sounds.PUFF );
+
+        hero.next();
+        wep.afterAbilityUsed(hero);
     }
 
 }
