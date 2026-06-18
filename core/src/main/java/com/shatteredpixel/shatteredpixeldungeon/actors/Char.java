59081b1429af0308e98074ddab021d2cd2922091
JDSA Ling <CutelinXXX@126.com>
2026-06-18 19:19:54 +0800

    v0.9.4.8：现在真实伤害将会破盾

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -1084,7 +1084,7 @@ public abstract class Char extends Actor {
 
 		int shielded = dmg;
 		//FIXME: when I add proper damage properties, should add an IGNORES_SHIELDS property to use here.
-		if (!(src instanceof Hunger) || !(type == DamageType.REAL)){
+		if (!(src instanceof Hunger) && !(type == DamageType.REAL)){
 			for (ShieldBuff s : buffs(ShieldBuff.class)){
 				dmg = s.absorbDamage(dmg);
 				if (dmg == 0) break;
