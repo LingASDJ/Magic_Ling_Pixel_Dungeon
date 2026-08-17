c9dee4cb949e913639828dd54b5d9f9fa2b2c5b6
qingxianda <1451798378@qq.com>
2026-08-18 00:28:50 +0800

    净化飞镖现在只对莲娜的盾不能删除，其他人的盾依然可以删除 闪电花现在回复血量的恢复至调整为最大血量的10%向上取整 闪电花与辉耀飞镖现在对我方怪物造成精英化效果时该效果存在500回合的时间限制

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/plants/AikeLaier.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/plants/AikeLaier.java
@@ -4,6 +4,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ChampionEnemy;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ChampionHero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FlavourBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Healing;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
@@ -11,6 +12,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfHealing;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.utils.Bundle;
 import com.watabou.utils.Random;
 
 public class AikeLaier extends Plant {
@@ -25,30 +27,66 @@ public class AikeLaier extends Plant {
 
         if (ch != null) {
             if (ch instanceof Mob) {
+                Class<? extends ChampionEnemy> championClass;
                 switch (Random.Int(4)) {
                     case 0:
                     default:
-                        Buff.affect(ch, ChampionEnemy.Blazing.class);
+                        championClass = ChampionEnemy.Blazing.class;
                         break;
                     case 1:
-                        Buff.affect(ch, ChampionEnemy.Projecting.class);
+                        championClass = ChampionEnemy.Projecting.class;
                         break;
                     case 2:
-                        Buff.affect(ch, ChampionEnemy.Blessed.class);
+                        championClass = ChampionEnemy.Blessed.class;
                         break;
                     case 3:
-                        Buff.affect(ch, ChampionEnemy.Halo.class);
+                        championClass = ChampionEnemy.Halo.class;
                         break;
+                }
+                Buff.affect(ch, championClass);
+
+                //我方(友方)怪物获得的精英强化改为限时效果
+                if (ch.alignment == Char.Alignment.ALLY) {
+                    ChampionTimeLimit limit = Buff.affect(ch, ChampionTimeLimit.class, 500);
+                    limit.championClass = championClass;
                 }
             } else if (ch instanceof Hero){
                 GLog.i( Messages.get(this, "refreshed") );
                 PotionOfHealing.cure(ch);
                 Buff.affect(ch, ChampionHero.Light.class, ChampionHero.DURATION/5);
-                Buff.affect(ch, Healing.class).setHeal(10, 5f, 6);
+                Buff.affect(ch, Healing.class).setHeal((int)Math.ceil(ch.HT * 0.1f), 5f, 6);
             }
         }
     }
 
+
+    //限时移除友方怪物身上的精英强化
+    public static class ChampionTimeLimit extends FlavourBuff {
+        public Class<? extends ChampionEnemy> championClass;
+
+        @Override
+        public boolean act() {
+            if (championClass != null) {
+                Buff.detach(target, championClass);
+            }
+            super.act();
+            return true;
+        }
+
+        private static final String CHAMPION_CLASS = "championClass";
+
+        @Override
+        public void storeInBundle(Bundle bundle) {
+            super.storeInBundle(bundle);
+            bundle.put(CHAMPION_CLASS, championClass);
+        }
+
+        @Override
+        public void restoreFromBundle(Bundle bundle) {
+            super.restoreFromBundle(bundle);
+            championClass = bundle.getClass(CHAMPION_CLASS);
+        }
+    }
     public static class Seed extends Plant.Seed {
         {
             image = ItemSpriteSheet.SEED_AIKELAIER;
