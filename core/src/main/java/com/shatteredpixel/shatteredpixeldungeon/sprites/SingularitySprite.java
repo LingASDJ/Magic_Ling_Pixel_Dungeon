98ae2be783477d24c92d9083668630a23410ce59
JDSA Ling <CutelinXXX@126.com>
2026-09-14 18:12:05 +0800

    v0.9.6.0：死灵骑兵动画优化，奇点，剑咒图标完成

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SingularitySprite.java
@@ -0,0 +1,31 @@
+package com.shatteredpixel.shatteredpixeldungeon.sprites;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.watabou.noosa.MovieClip;
+import com.watabou.noosa.TextureFilm;
+
+public class SingularitySprite extends MobSprite {
+
+    public SingularitySprite() {
+        super();
+
+        texture( Assets.Sprites.ANIMATIONS_DARKSUN );
+
+        TextureFilm frames = new TextureFilm( texture, 24, 24 );
+
+        idle = new MovieClip.Animation( 12, true );
+        idle.frames( frames, 0,1,2,3,4,5,6,7 );
+
+        run = new MovieClip.Animation( 12, true );
+        run.frames( frames, 0,1,2,3,4,5,6,7 );
+
+        attack = new MovieClip.Animation( 12, false );
+        attack.frames( frames, 0,1,2,3,4,5,6,7 );
+
+        die = new MovieClip.Animation( 12, false );
+        die.frames( frames, 0,1,2,3,4,5,6,7 );
+
+        play( idle );
+    }
+}
+
