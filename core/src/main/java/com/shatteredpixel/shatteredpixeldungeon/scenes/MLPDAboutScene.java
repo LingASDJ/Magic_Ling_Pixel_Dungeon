54fecff3dfc1333fab6d8d65b93d53053a6c46da
chocosuki <chocosuki81@gmail.com>
2026-09-22 00:57:49 +0800

    v0960：重新补

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/MLPDAboutScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/MLPDAboutScene.java
@@ -129,6 +129,18 @@ public class MLPDAboutScene extends PixelScene {
                 left = 16;
                 top = 48;
                 break;
+            case "OMICRONG":
+                left = 32;
+                top = 48;
+                break;
+            case "ROBEIRI":
+                left = 48;
+                top = 48;
+                break;
+            case "NXHY":
+                left = 64;
+                top = 48;
+                break;    
         }
         return new Image(Assets.Interfaces.ABOUT_ICONS,left,top,16,16);
     }
@@ -398,7 +410,7 @@ public class MLPDAboutScene extends PixelScene {
         CreditsBlock openration1 = new CreditsBlock(true, 0x4ecdc4,
                 "运 营 宣 发" +
                         "\n",
-                Icons.BACKPACK.get(),
+                About_Icons("ROBEIRI"),
                 "罗贝里",
                 null,
                 null);
@@ -407,7 +419,7 @@ public class MLPDAboutScene extends PixelScene {
 
         CreditsBlock openration2 = new CreditsBlock(true, 0xa884ec,
                 "",
-                About_Icons("JISHEN"),
+                About_Icons("NXHY"),
                 "那些回忆",
                 null,
                 null);
@@ -426,7 +438,7 @@ public class MLPDAboutScene extends PixelScene {
 
         CreditsBlock openration4 = new CreditsBlock(true, 0xa884ec,
                 "",
-                About_Icons("JISHEN"),
+                About_Icons("OMICRONG"),
                 "omicronrg9",
                 null,
                 null);
