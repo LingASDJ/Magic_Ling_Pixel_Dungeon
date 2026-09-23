a015cb9622725ca92fdb17836882ac3dc1e57e4e
JDSA Ling <CutelinXXX@126.com>
2026-09-23 22:07:26 +0800

    V0.9.6.0：安卓版崩溃日志中介界面

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -1,7 +1,7 @@
 package com.shatteredpixel.shatteredpixeldungeon.android;
 
 public class AndroidLauncher {
-    public static void showNativeCrashDialog(String string) {
+    public static void showNativeCrashDialog(String string,String string2) {
         //安卓多架构 桌面端不支持 为此添加傀儡Java类代替
     }
 }
